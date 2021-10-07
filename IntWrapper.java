b8181023d9c4dc38652c3ef9e34d095a8af203b3
Yordan Iliev <jordan.iliev501@gmail.com>
2021-10-07 17:43:17 +0300

    Add int wrapper class

---
+++ IntWrapper.java
@@ -0,0 +1,15 @@
+public class IntWrapper {
+	private int value;
+	
+	public IntWrapper(int v) {
+		value = v;
+	}
+	
+	public void setValue(int v) {
+		value = v;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
